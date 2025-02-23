fffd2f73c42782578e21502839786cebbba79c3c
hasanalbanna012 <banna15-5064@diu.edu.bd>
2025-02-23 12:36:56 +0600

    task2

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -11,24 +11,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("Advanced UI"),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CachedNetworkImage(
-              imageUrl:
-                  "https://i.pinimg.com/736x/ed/bc/74/edbc74d00fdfab468779d47c79a2cef8.jpg",
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-            ),
-          ],
+      home: Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items:[
+          BottomNavigationBarItem(icon: Icon(Icons.home),
+          label: "Home",),
+        ] ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                height: 70,
+                child: DrawerHeader(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.facebook),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Icon(Icons.close),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                trailing: ElevatedButton(onPressed: () {}, child: Text("theme"),),
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CachedNetworkImage(
+                imageUrl:
+                    "https://i.pinimg.com/736x/ed/bc/74/edbc74d00fdfab468779d47c79a2cef8.jpg",
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
